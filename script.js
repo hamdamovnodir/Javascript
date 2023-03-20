845412483b8a4ef47e3d22fdeaa015dae3f9fa00
Nodir <khamdamovnodir784gmail.com>
2023-03-20 16:07:05 +0500

    javascrip fayl

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// let number = 15
+// console.log(number)
+// number = 100
+// console.log(number)
+
+// let num=100
+// let son=200
+// console.log(++num+ ++son)
+
+// const boy=1
+
+if (boy.count > 2 && boy.count < 6) {
+    console.log("This is childhood")
+} else if (boy.count > 7 && boy.count < 16) {
+    console.log("This is school ")
+} else if (boy.count > 16 && boy.count < 21) {
+    console.log("This is university ")
+}
+
+
+const color = "red"
+
+switch (color) {
+    case "red":
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("Slowly");
+        break;
+    case "Green":
+        console.log("Goo");
+        break;
+    default:
+        console.log("Traffic jam");
+        break;
+}
